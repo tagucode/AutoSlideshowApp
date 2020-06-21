a41069a865a8ad500499aef4a6eabcf7469d3150
tagucode <silverxclock@gmail.com>
2020-06-21 21:31:45 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/satoshi/autoslideshowapp/MainActivity.kt
@@ -0,0 +1,86 @@
+package jp.techacademy.satoshi.autoslideshowapp
+
+import android.Manifest
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import android.view.View
+import android.provider.MediaStore
+import android.content.ContentUris
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(),View.OnClickListener {
+
+    private val PERMISSION_REQUEST_CODE = 100
+
+    val resolver = contentResolver
+    val cursor = resolver.query(
+        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, //データの種類
+        null, // 項目(null = 全項目)
+        null, // フィルタ条件(null = フィルタなし)
+        null, // フィルタ用パラメータ
+        null  // ソート (null ソートなし)
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        nextButton.setOnClickListener(this)
+        playButton.setOnClickListener(this)
+        backButton.setOnClickListener(this)
+
+        // ANDROID 6.0以降の場合
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //パーミッションの許可状態を確認する
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                // 許可されている
+                getContentsInfo()
+            } else {
+                // 許可されていないので許可ダイアログを表示する
+                requestPermissions(
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    PERMISSION_REQUEST_CODE
+                )
+            }
+            // Android 5系以下の場合
+        } else {
+            getContentsInfo()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSION_REQUEST_CODE ->
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getContentsInfo()
+                }
+        }
+    }
+
+    private fun getContentsInfo() {
+
+        if (cursor!!.moveToFirst()) {
+            // indexからIDを取得し、そのIDから画像のURIを取得する
+            val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+            val id = cursor.getLong(fieldIndex)
+            val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+            imageView.setImageURI(imageUri)
+        }
+        cursor.close()
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.playButton -> getContentsInfo()
+        }
+    }
+
+}
