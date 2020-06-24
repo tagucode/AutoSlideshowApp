7effe17e172fac537d97ad5a6b17d7ca9928b5ca
tagucode <silverxclock@gmail.com>
2020-06-24 20:02:19 +0900

    一時保存

--- app/src/main/java/jp/techacademy/satoshi/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/satoshi/autoslideshowapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package jp.techacademy.satoshi.autoslideshowapp
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.ContentResolver
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.content.pm.PackageManager
@@ -9,24 +11,29 @@ import android.util.Log
 import android.view.View
 import android.provider.MediaStore
 import android.content.ContentUris
+import android.database.Cursor
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(),View.OnClickListener {
 
     private val PERMISSION_REQUEST_CODE = 100
 
-    val resolver = contentResolver
-    val cursor = resolver.query(
-        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, //データの種類
-        null, // 項目(null = 全項目)
-        null, // フィルタ条件(null = フィルタなし)
-        null, // フィルタ用パラメータ
-        null  // ソート (null ソートなし)
-    )
+    lateinit var resolver:ContentResolver
+    var cursor:Cursor? = null
 
+    @SuppressLint("Recycle")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        resolver = contentResolver
+        cursor = resolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI, //データの種類
+            null, // 項目(null = 全項目)
+            null, // フィルタ条件(null = フィルタなし)
+            null, // フィルタ用パラメータ
+            null  // ソート (null ソートなし)
+        )
 
         nextButton.setOnClickListener(this)
         playButton.setOnClickListener(this)
@@ -68,13 +75,13 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
 
         if (cursor!!.moveToFirst()) {
             // indexからIDを取得し、そのIDから画像のURIを取得する
-            val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
-            val id = cursor.getLong(fieldIndex)
+            val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+            val id = cursor!!.getLong(fieldIndex)
             val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
 
             imageView.setImageURI(imageUri)
         }
-        cursor.close()
+        cursor!!.close()
     }
 
     override fun onClick(v: View) {
